91e6ba087d773cc6def16efed3976d3cbbedae18
ygt-ernsy <yigiterensoy@hotmail.com>
2025-10-21 16:35:32 +0300

    initial file strutcture

---
+++ src/EsportsManagmentApp.java
@@ -0,0 +1,8 @@
+/**
+ * EsportsManagmentApp
+ */
+public class EsportsManagmentApp {
+	public static void main(String[] args) {
+		// TODO: Homework
+	}
+}
---
+++ src/Game.java
@@ -0,0 +1,5 @@
+class Game {
+	private int id;
+	private String name;
+	private int basePointPerRound;
+}
