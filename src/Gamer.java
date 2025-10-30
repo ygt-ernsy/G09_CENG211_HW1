b64685fe6c9397e6017feb9a29919b49dfca4593
ygt-ernsy <yigiterensoy@hotmail.com>
2025-10-30 10:52:52 +0300

    Enhance documentation in Gamer class
    Updated comments for clarity and added Javadoc for constructors and methods.

--- src/Gamer.java
+++ src/Gamer.java
@@ -1,23 +1,39 @@
 /**
  * Gamer
+ * 
+ * Represents a gamer/user with identifying information and experience details.
  */
 public class Gamer {
-	private int id;
-	private String nickname;
-	private String name;
-	private String phoneNumber;
-	private int experianceYears;
+	private int id;                 // Unique identifier for the gamer
+	private String nickname;        // Gamer's nickname
+	private String name;            // Gamer's real name
+	private String phoneNumber;     // Gamer's phone number
+	private int experianceYears;    // Number of years of experience
 
+	/**
+	 * Default constructor initializes fields to default values.
+	 */
 	public Gamer() {
 		this(0, null, null, null, 0);
 	}
 
-	// Copy constructor
+	/**
+	 * Copy constructor creates a new Gamer from another Gamer object.
+	 * @param gamer The gamer instance to copy
+	 */
 	public Gamer(Gamer gamer) {
 		this(gamer.getId(), gamer.getNickname(), gamer.getName(), gamer.getPhoneNumber(),
 				gamer.getExperianceYears());
 	}
 
+	/**
+	 * Constructs a Gamer with all fields specified.
+	 * @param id Unique identifier for the gamer
+	 * @param nickname Gamer's nickname
+	 * @param name Gamer's real name
+	 * @param phoneNumber Gamer's phone number
+	 * @param experianceYears Number of years of experience
+	 */
 	public Gamer(int id, String nickname, String name, String phoneNumber, int experianceYears) {
 		this.id = id;
 		this.nickname = nickname;
@@ -26,29 +42,47 @@ public class Gamer {
 		this.experianceYears = experianceYears;
 	}
 
+	/**
+	 * @return The gamer's unique identifier
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @return The gamer's nickname
+	 */
 	public String getNickname() {
 		return nickname;
 	}
 
+	/**
+	 * @return The gamer's real name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return The gamer's phone number
+	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
+	/**
+	 * @return The number of years of experience
+	 */
 	public int getExperianceYears() {
 		return experianceYears;
 	}
 
+	/**
+	 * Returns a string representation of the Gamer object.
+	 */
 	@Override
 	public String toString() {
-		return "Id: " + id + " Nickname: " + nickname + " Name: " + " PhoneNumber: " + " ExperianceYears: "
-				+ experianceYears;
+		return "Id: " + id + " Nickname: " + nickname + " Name: " + name + " PhoneNumber: " + phoneNumber +
+				" ExperianceYears: " + experianceYears;
 	}
 }
