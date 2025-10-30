a460ed7246ca170d26bd920f4d133da719d0817f
ygt-ernsy <yigiterensoy@hotmail.com>
2025-10-30 16:45:46 +0300

    Reset PointsBoard class

--- src/PointsBoard.java
+++ src/PointsBoard.java
@@ -2,14 +2,5 @@
  * PointsBoard
  */
 public class PointsBoard {
-	private final Gamer[] gamers;
-	private Match[][] matches;
-	private final int[] totalPoints;
-	private final double[] averagePointPerMatch;
-	private final String[] gamerMedal;
 
-	public PointsBoard(Gamer[] gamers, Match[][] matches) {
-		this.gamers = gamers;
-		this.matches = matches;
-	}
 }
