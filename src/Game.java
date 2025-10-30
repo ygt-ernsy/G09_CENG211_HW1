2609cbb4016a551a6a6d95e248611aac23a67f25
ygt-ernsy <yigiterensoy@hotmail.com>
2025-10-30 22:53:12 +0300

    Added missing public field to Game class

--- src/Game.java
+++ src/Game.java
@@ -1,46 +1,46 @@
-class Game {
-	// Unique identifier for the game
-	private int id;
-	// Name of the game
-	private String name;
-	// Base points awarded per round in the game
-	private int basePointPerRound;
+public class Game {
+    // Unique identifier for the game
+    private int id;
+    // Name of the game
+    private String name;
+    // Base points awarded per round in the game
+    private int basePointPerRound;
 
-	// Default constructor initializing fields to default values
-	public Game() {
-		this(0, null, 0);
-	}
+    // Default constructor initializing fields to default values
+    public Game() {
+        this(0, null, 0);
+    }
 
-	// Copy constructor to create a new Game object with the same values as another
-	// Game object
-	public Game(Game game) {
-		this(game.getId(), game.getName(), game.getBasePointPerRound());
-	}
+    // Copy constructor to create a new Game object with the same values as another
+    // Game object
+    public Game(Game game) {
+        this(game.getId(), game.getName(), game.getBasePointPerRound());
+    }
 
-	// Parameterized constructor to initialize Game with specific values
-	public Game(int id, String gameName, int basePointPerRound) {
-		this.id = id;
-		this.name = gameName;
-		this.basePointPerRound = basePointPerRound;
-	}
+    // Parameterized constructor to initialize Game with specific values
+    public Game(int id, String gameName, int basePointPerRound) {
+        this.id = id;
+        this.name = gameName;
+        this.basePointPerRound = basePointPerRound;
+    }
 
-	// Getter for the game ID
-	public int getId() {
-		return id;
-	}
+    // Getter for the game ID
+    public int getId() {
+        return id;
+    }
 
-	// Getter for the game name
-	public String getName() {
-		return name;
-	}
+    // Getter for the game name
+    public String getName() {
+        return name;
+    }
 
-	// Getter for the base points per round
-	public int getBasePointPerRound() {
-		return basePointPerRound;
-	}
+    // Getter for the base points per round
+    public int getBasePointPerRound() {
+        return basePointPerRound;
+    }
 
-	// Returns a string representation of the Game object
-	public String toString() {
-		return "Id: " + id + " Name: " + name + " BasePoingPerRound: " + basePointPerRound;
-	}
+    // Returns a string representation of the Game object
+    public String toString() {
+        return "Id: " + id + " Name: " + name + " BasePoingPerRound: " + basePointPerRound;
+    }
 }
